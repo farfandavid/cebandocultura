238f43d78000448693d8fb0ebb5762b0b1726e8f
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2023-11-09 00:14:24 -0300

    Agregando vista search products

---
+++ cebandocultura-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchProduct from './components/SearchProduct';
+import './styles/SearchProduct.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+
+      <header className="App-header">
+        <SearchProduct></SearchProduct>
+      </header>
+    </div>
+  );
+}
+
+export default App;
